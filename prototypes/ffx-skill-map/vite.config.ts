976bbf282fbce3e178c67e475848f4b38712a830
nsuberi <nsuberi@gmail.com>
2025-07-12 09:07:23 -0600

    attempt to fix access denied errors when navigating from the portfolio homepage to the ffx skill map demo

--- prototypes/ffx-skill-map/vite.config.ts
+++ prototypes/ffx-skill-map/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/prototypes/ffx-skill-map/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
