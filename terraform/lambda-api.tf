853903e7a1896fa9985096524ee76bb66e02984c
nsuberi <nsuberi@gmail.com>
2025-08-05 22:31:14 -0600

    try using the lambda function directly instead of the api gateway

--- terraform/lambda-api.tf
+++ terraform/lambda-api.tf
@@ -238,6 +238,7 @@ resource "aws_api_gateway_deployment" "ai_api_deployment" {
       aws_api_gateway_integration.options_integration.id,
       aws_api_gateway_method_response.options_response.id,
       aws_api_gateway_integration_response.options_integration_response.id,
+      "force-cors-update-2025-08-06",
     ]))
   }
 
