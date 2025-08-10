9fcc8120a053b026d3ccb0e40a81313be89bfeb9
nsuberi <nsuberi@gmail.com>
2025-08-10 11:20:08 -0600

    further test updates

--- prototypes/ffx-skill-map/jest.config.cjs
+++ prototypes/ffx-skill-map/jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/?(*.)+(test).[tj]s?(x)'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/**/*.integration.test.(ts|tsx|js|jsx)'
+  ],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
     '^.+\\.(js|jsx)$': 'babel-jest'
