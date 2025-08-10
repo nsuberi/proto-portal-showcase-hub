85797afd0532727d5ca440857093f3919ad5521a
nsuberi <nsuberi@gmail.com>
2025-08-10 13:11:51 -0600

    updates to integration testing

--- prototypes/ffx-skill-map/debug-images.js
+++
@@ -1,9 +0,0 @@
-// Debug script to run in browser console
-// This will clear localStorage and show what image paths are being loaded
-
-console.log('🧹 Clearing localStorage...');
-localStorage.removeItem('ffx-skill-map-employees');
-localStorage.removeItem('tech-skill-map-employees');
-
-console.log('🔄 Refreshing page to load fresh employee data...');
-window.location.reload();
